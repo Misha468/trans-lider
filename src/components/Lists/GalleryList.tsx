9ba8958ff890ca3eff8c3edc62bf3e8055e471c3
Misha468 <mixa102006@ya.ru>
2025-07-03 16:26:29 +0300

    upd gallery

--- src/components/Lists/GalleryList.tsx
+++ src/components/Lists/GalleryList.tsx
@@ -1,25 +1,19 @@
-import team from "../../assets/static/galleryPhotos/team.png";
 import technic1 from "../../assets/static/galleryPhotos/technic1.png";
 import technic2 from "../../assets/static/galleryPhotos/technic2.png";
 import technic3 from "../../assets/static/galleryPhotos/technic3.png";
 const GalleryList = [
   {
     id: 1,
-    image: team,
-    title: "Наша команда",
-  },
-  {
-    id: 2,
     image: technic1,
     title: "Кран-манипулятор на базе КАМАЗ и автовышка",
   },
   {
-    id: 3,
+    id: 2,
     image: technic2,
     title: "Экскаватор-погрузчик JCB за работой",
   },
   {
-    id: 4,
+    id: 3,
     image: technic3,
     title: "Экскаватор-погрузчик JCB за работой",
   },
